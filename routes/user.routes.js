bce04917ad8fdb5a064bfe2685d40c5d5d10ca40
nitin kumar <nitin981275@gmail.com>
2026-02-16 17:58:09 +0530

    Add 404 checks for missing user in CRUD routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,6 +11,12 @@ router.post(
     const { name, age } = req.body;
     const user = await User.create({ name, age });
 
+    if (!name || !age) {
+      const error = new Error("name and age does not found!");
+      error.statusCode = 404;
+      throw error;
+    }
+
     res.status(200).json({
       message: `Created new user : ${name}`,
       data: user,
@@ -23,6 +29,13 @@ router.get(
   "/",
   asyncHandler(async (req, res, next) => {
     const user = await User.find();
+
+    if (!user) {
+      const error = new Error("No Data available");
+      error.statusCode = 404;
+      throw error;
+    }
+
     res.status(200).json({
       message: "Fetched all users.",
       data: user,
@@ -36,6 +49,11 @@ router.get(
   asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.params.id);
 
+    if (!user) {
+      const error = new Error("User not found!");
+      error.statusCode = 404;
+      throw error;
+    }
     res.status(200).json({
       message: "data fetched",
       data: user,
@@ -49,14 +67,17 @@ router.put(
   asyncHandler(async (req, res) => {
     const id = req.params.id;
     const updateuser = req.body;
+
     const updatedUser = await User.findByIdAndUpdate(id, updateuser, {
       new: true,
     });
-    if (!updatedUser) {
-      return res.status(404).json({
-        message: "User not found.",
-      });
+
+    if (!req.body) {
+      const error = new Error("request body is empty.");
+      error.statusCode = 400;
+      throw error;
     }
+
     res.status(200).json({
       message: "User updated Successfully",
       data: updatedUser,
@@ -69,6 +90,12 @@ router.delete(
   "/:id",
   asyncHandler(async (req, res) => {
     const deleteuser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deleteuser) {
+      const error = new Error("User does not exist.");
+      error.statusCode = 404;
+      throw error;
+    }
     res.status(200).json({
       message: "User deleted successfully!",
       data: deleteuser,
